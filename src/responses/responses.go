debade92fe99dfa6f5d967c71568403713aa5a28
Igor Silva <danieligord7@hotmail.com>
2023-01-25 22:05:14 -0300

    API: Adding a function to handle responses..

---
+++ src/responses/responses.go
@@ -0,0 +1,25 @@
+package responses
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// Format response to JSON format and handle response statusCode
+func FormatResponseToJSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.WriteHeader(statusCode)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Format error to JSON based on FormatResponseToJSON
+func FormatResponseToCustomError(w http.ResponseWriter, statusCode int, err error) {
+	FormatResponseToJSON(w, statusCode, struct {
+		Err string `json:"error"`
+	}{
+		Err: err.Error(),
+	})
+}
