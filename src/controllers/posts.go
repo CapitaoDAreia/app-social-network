560f1478a223e04f766ec85d8419f5e546990813
CapitaoDAreia <danieligord7@hotmail.com>
2023-02-04 22:52:14 -0300

    API: Implementing SearchPost route.

--- src/controllers/posts.go
+++ src/controllers/posts.go
@@ -9,6 +9,9 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 // --
@@ -63,7 +66,28 @@ func GetPosts(w http.ResponseWriter, r *http.Request) {
 
 // --
 func GetPost(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	postID, err := strconv.ParseUint(params["postId"], 10, 64)
+	if err != nil {
+		responses.FormatResponseToCustomError(w, http.StatusBadRequest, err)
+		return
+	}
 
+	DB, err := database.ConnectWithDatabase()
+	if err != nil {
+		responses.FormatResponseToCustomError(w, 500, err)
+		return
+	}
+	defer DB.Close()
+
+	repository := repository.NewPostsRepository(DB)
+	post, err := repository.SearchPost(postID)
+	if err != nil {
+		responses.FormatResponseToCustomError(w, 500, err)
+		return
+	}
+
+	responses.FormatResponseToJSON(w, 200, post)
 }
 
 // --
--- src/repository/posts.go
+++ src/repository/posts.go
@@ -32,3 +32,33 @@ func (p postsRepository) CreatePost(post models.Post) (uint64, error) {
 
 	return uint64(lastId), nil
 }
+
+func (p postsRepository) SearchPost(postID uint64) (models.Post, error) {
+	rows, err := p.db.Query(`
+		 select p.*, u.nick from
+		 posts p inner join users u
+		 on u.id = p.authorId where p.id = ?
+	`, postID)
+	if err != nil {
+		return models.Post{}, err
+	}
+	defer rows.Close()
+
+	var post models.Post
+
+	if rows.Next() {
+		if err := rows.Scan(
+			&post.ID,
+			&post.Title,
+			&post.Content,
+			&post.AuthorID,
+			&post.Likes,
+			&post.CreatedAt,
+			&post.AuthorNick,
+		); err != nil {
+			return models.Post{}, err
+		}
+	}
+
+	return post, nil
+}
