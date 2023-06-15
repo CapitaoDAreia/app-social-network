11496567952c88a86b6fca4f09bf4bc9c481f507
CapitaoDAreia <danieligord7@hotmail.com>
2023-06-14 21:56:46 -0300

    Refactoring controllers test: CreateUser

--- backend/internal/application/services/mocks/users.go
+++ backend/internal/application/services/mocks/users.go
@@ -14,9 +14,9 @@ func NewUsersServiceMock() *UsersServiceMock {
 	return &UsersServiceMock{}
 }
 
-func (service *UsersServiceMock) CreateUser(user entities.User) (uint64, error) {
+func (service *UsersServiceMock) CreateUser(user entities.User) (string, error) {
 	args := service.Called(user)
-	return args.Get(0).(uint64), args.Error(1)
+	return args.Get(0).(string), args.Error(1)
 }
 
 func (service *UsersServiceMock) SearchUsers(usernameOrNickQuery string) ([]entities.User, error) {
@@ -24,19 +24,19 @@ func (service *UsersServiceMock) SearchUsers(usernameOrNickQuery string) ([]enti
 	return args.Get(0).([]entities.User), args.Error(1)
 }
 
-func (service *UsersServiceMock) SearchUser(requestID uint64) (entities.User, error) {
+func (service *UsersServiceMock) SearchUser(requestID string) (entities.User, error) {
 	args := service.Called(requestID)
 	return args.Get(0).(entities.User), args.Error(1)
 }
 
-func (service *UsersServiceMock) UpdateUser(ID uint64, user entities.User) error {
+func (service *UsersServiceMock) UpdateUser(ID string, user entities.User) (uint64, error) {
 	args := service.Called(ID, user)
-	return args.Error(0)
+	return args.Get(0).(uint64), args.Error(1)
 }
 
-func (service *UsersServiceMock) DeleteUser(ID uint64) error {
+func (service *UsersServiceMock) DeleteUser(ID string) (uint64, error) {
 	args := service.Called(ID)
-	return args.Error(0)
+	return args.Get(0).(uint64), args.Error(1)
 }
 
 func (service *UsersServiceMock) SearchUserByEmail(email string) (entities.User, error) {
@@ -44,32 +44,32 @@ func (service *UsersServiceMock) SearchUserByEmail(email string) (entities.User,
 	return args.Get(0).(entities.User), args.Error(1)
 }
 
-func (service *UsersServiceMock) Follow(followedID, followerID uint64) error {
+func (service *UsersServiceMock) Follow(followedID, followerID string) error {
 	args := service.Called(followedID, followerID)
 	return args.Error(0)
 }
 
-func (service *UsersServiceMock) UnFollow(followedID, followerID uint64) error {
+func (service *UsersServiceMock) UnFollow(followedID, followerID string) error {
 	args := service.Called(followedID, followerID)
 	return args.Error(0)
 }
 
-func (service *UsersServiceMock) SearchFollowersOfAnUser(userID uint64) ([]entities.User, error) {
+func (service *UsersServiceMock) SearchFollowersOfAnUser(userID string) ([]string, error) {
 	args := service.Called(userID)
-	return args.Get(0).([]entities.User), args.Error(1)
+	return args.Get(0).([]string), args.Error(1)
 }
 
-func (service *UsersServiceMock) SearchWhoAnUserFollow(userID uint64) ([]entities.User, error) {
+func (service *UsersServiceMock) SearchWhoAnUserFollow(userID string) ([]string, error) {
 	args := service.Called(userID)
-	return args.Get(0).([]entities.User), args.Error(1)
+	return args.Get(0).([]string), args.Error(1)
 }
 
-func (service *UsersServiceMock) SearchUserPassword(userID uint64) (string, error) {
+func (service *UsersServiceMock) SearchUserPassword(userID string) (string, error) {
 	args := service.Called(userID)
 	return args.Get(0).(string), args.Error(1)
 }
 
-func (service *UsersServiceMock) UpdateUserPassword(requestUserId uint64, hashedNewPasswordStringed string) error {
+func (service *UsersServiceMock) UpdateUserPassword(requestUserId string, hashedNewPasswordStringed string) (uint64, error) {
 	args := service.Called(requestUserId, hashedNewPasswordStringed)
-	return args.Error(0)
+	return args.Get(0).(uint64), args.Error(1)
 }
